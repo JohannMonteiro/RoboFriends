55c597e946e276606e56e8323c0e1ee364315f5c
Johann Monteiro <monteiro.johann@gmail.com>
2023-06-19 11:15:06 -0300

    Styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 import { render } from "@testing-library/react";
-
+import "./App.css";	
 class App extends Component {
   constructor() {
     super();
@@ -24,7 +24,7 @@ class App extends Component {
     })
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
